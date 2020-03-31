eb8745511026f484c920e3ccc52774ef9af68f76
eomain <eomain@protonmail.com>
2020-03-31 22:46:23 +0100

    surface objects

--- src/surface/mod.rs
+++ src/surface/mod.rs
@@ -5,15 +5,32 @@
 #[allow(unused_imports)]
 
 use std::collections::HashMap;
+use crate::object::*;
 
 /// A position on the surface
 pub type Position = (usize, usize);
 
 pub type Map<K, V> = HashMap<K, V>;
 
+#[derive(Debug, Clone, PartialEq, Hash)]
+pub enum Object {
+    Point(Point),
+    Line(Line),
+    Rect(Rect)
+}
+
 /// A surface contains a set of object
 /// that are intented to be displayed.
-#[derive(Clone, Hash)]
+#[derive(Debug, Clone, PartialEq, Hash)]
 pub struct Surface {
+    objects: Vec<Object>
+}
 
+impl Surface {
+    pub fn new(objects: Vec<Object>) -> Self
+    {
+        Self {
+            objects
+        }
+    }
 }
