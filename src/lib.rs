cb4f5783f8418f6068d623e30e3e34464a40f959
eomain <eomain@protonmail.com>
2020-03-31 07:38:03 +0100

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # Mirage
 //! Mirage is a library for describing vector graphics.
 //! Rather than actually rendering the graphics itself,
-//! it is intended to provide a usable defintion that can be used
+//! it is intended to provide a usable definition that can be used
 //! by external rendering programs themselves.
 
 pub mod object;
